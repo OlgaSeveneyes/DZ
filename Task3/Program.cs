077984cb85aa643c8e5dc02ef330c9af4b6d8461
Olga <osa_007@mail.ru>
2022-08-12 00:06:23 +0300

    added task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+string? N = Console.ReadLine();
+int Ni;
+
+if (int.TryParse(N, out Ni))
+{
+    if (Ni % 2 == 0)
+    {
+      Console.WriteLine("Четное");  
+    }
+    else 
+    {
+      Console.WriteLine("Нечетное");    
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+} 
