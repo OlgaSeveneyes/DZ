b06d4ba6caaedca7de03300b05f2293048c94fb3
Olga <osa_007@mail.ru>
2022-08-12 00:11:40 +0300

    added task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+string? N = Console.ReadLine();
+int Ni;
+int K = 0;
+
+if (int.TryParse(N, out Ni))
+{
+    if (Ni % 2 == 0)
+    {
+     while (Ni > K)
+      {
+        K = K + 2;
+        Console.WriteLine(K);  
+      }
+    }
+    else 
+    {
+       while (Ni-1 > K)
+       {
+        K = K + 2;
+        Console.WriteLine(K);
+       } 
+    } 
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+} 
+
+
