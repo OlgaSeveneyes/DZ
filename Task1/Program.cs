5b3fd5e60401cf49ff5e57e8bc00b78d63b7569b
Olga <osa_007@mail.ru>
2022-08-11 23:42:52 +0300

    added task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+string? number1S = Console.ReadLine();
+double number1D;
+
+Console.Write("Введите второе число: ");
+string? number2S = Console.ReadLine();
+double number2D;
+
+if (double.TryParse(number1S, out number1D))
+{
+    if (double.TryParse(number2S, out number2D))
+    {
+        if (number1D > number2D)
+        {
+           Console.WriteLine(number1D); 
+        }
+        else
+        {
+            Console.WriteLine(number2D);
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода! Необходимо ввести число");
+} 
